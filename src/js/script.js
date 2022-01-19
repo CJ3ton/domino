618eda4e4e06e633c934c475a5ec2e867921359f
Кобцев Денис Анатольевич <admin@gora.slavgorod.altai.ru>
2022-01-19 21:56:28 +0700

    Domino items textures added

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,9 @@ const board = document.querySelector('.game__board'),
     dominos = board.querySelector('.game__board-dominos'),
     domino = dominos.querySelectorAll('.domino');
 
-const defaultArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28];
+const defaultArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27];
+const boardSet = shuffle(defaultArr);
+const dominoSet = [];
 
 function dominoItem(id, firstNum, secondNum, status, set) {
     this.id = id;
@@ -15,7 +17,15 @@ function dominoItem(id, firstNum, secondNum, status, set) {
     };
 }
 
-const domino1 = new dominoItem(1, 0, 1);
+function createDominoItems() {
+    let count = 0;
+    for (let i = 0; i < 7; i++) {
+        for (let j = i; j < 7; j++) {
+            dominoSet[count] = new dominoItem(count, i, j);
+            count++;
+        }
+    }
+}
 
 function shuffle(arr) {
     let j, temp;
@@ -28,11 +38,20 @@ function shuffle(arr) {
     return arr;
 }
 
-function gameInit() {
-    const boardSet = shuffle(defaultArr);
-    console.log(domino1);
-    domino1.open();
-    console.log(domino1);
+function dominoItemOpen(id) {
+    dominoSet[boardSet[id]].open();
 }
+
+function gameInit() {
+    createDominoItems();
+    //dominoItemUp(4);
+    console.log(dominoSet[4]);
+}
+
+dominos.addEventListener('click', (e) => {
+    const id = e.target.dataset.id - 1;
+    currentItem = dominoSet[boardSet[id]];
+    dominoItemOpen(id);
+});
 
 window.addEventListener('DOMContentLoaded', gameInit);
