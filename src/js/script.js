100a7eea13b9f4a54ebabd26519705fbfe6c6bdd
Кобцев Денис Анатольевич <admin@gora.slavgorod.altai.ru>
2022-01-20 17:01:26 +0700

    Module edit

--- src/js/script.js
+++ src/js/script.js
@@ -2,18 +2,22 @@ const board = document.querySelector('.game__board'),
     dominos = board.querySelector('.game__board-dominos'),
     domino = dominos.querySelectorAll('.domino');
 
-const defaultArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27];
-const boardSet = shuffle(defaultArr);
 const dominoSet = [];
+let activeItem = 0;
+const openItems = [0, 21, 22, 23, 24, 25, 26, 27];
 
 function dominoItem(id, firstNum, secondNum, status, set) {
     this.id = id;
     this.firstNum = firstNum;
     this.secondNum = secondNum;
-    this.status = 'down';
+    this.status = 0;
     this.set = true;
+    this.canOpen = false;
+    this.rotated = false;
+    this.active = false;
+    this.value = this.firstNum + this.secondNum;
     this.open = function () {
-        this.status = 'up';
+        this.status = 1;
     };
 }
 
@@ -38,20 +42,62 @@ function shuffle(arr) {
     return arr;
 }
 
+function boardRender() {
+    dominoSet.forEach((item, i) => {
+
+    });
+}
+
 function dominoItemOpen(id) {
-    dominoSet[boardSet[id]].open();
+    dominoSet[id].open();
+    domino[id].classList.add(`open-${dominoSet[id].id}`);
 }
 
 function gameInit() {
     createDominoItems();
-    //dominoItemUp(4);
-    console.log(dominoSet[4]);
+    shuffle(dominoSet);
+    openItems.forEach((item, i) => {
+        dominoItemOpen(item);
+    });
+    //boardRender();
+    console.log(dominoSet);
+}
+
+function dominoItemActivity(id) {
+    currentItem = dominoSet[id];
+    if (currentItem.status !== 0) {
+        if (!currentItem.active) {
+            if (activeItem == 0) {
+                domino[id].classList.add('active');
+                currentItem.active = true;
+                activeItem = id;
+            } else {
+                if (checkSum(activeItem, id)) {
+                    console.log('YES!');
+                }
+
+            }
+        } else {
+            currentItem.active = false;
+            activeItem = 0;
+            domino[id].classList.remove('active');
+        }
+
+    }
+    console.log(currentItem);
+}
+
+function checkSum(id1, id2) {
+    if (dominoSet[id1].value + dominoSet[id2].value === 12) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 dominos.addEventListener('click', (e) => {
-    const id = e.target.dataset.id - 1;
-    currentItem = dominoSet[boardSet[id]];
-    dominoItemOpen(id);
+    const id = e.target.dataset.id;
+    dominoItemActivity(id);
 });
 
 window.addEventListener('DOMContentLoaded', gameInit);
